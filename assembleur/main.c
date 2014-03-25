d0e96c50eb7aa894a3cb23abb23a3944432ecb4c
jobertomeu <jobertomeu@debian>
2014-03-25 13:17:04 +0100

    fi

--- assembleur/main.c
+++ assembleur/main.c
@@ -5,8 +5,29 @@
 ** Login   <jobertomeu@epitech.net>
 ** 
 ** Started on  Mon Mar 24 19:52:03 2014 Joris Bertomeu
-** Last update Mon Mar 24 19:57:18 2014 Joris Bertomeu
+** Last update Tue Mar 25 13:12:49 2014 Joris Bertomeu
 */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "gnl.h"
+
+void	tread_file(char *path)
+{
+  int	fd;
+  char	*buff;
+
+  buff = malloc(4096 * sizeof(*buff));
+  fd = open(path, O_RDONLY);
+  if (fd != -1)
+    {
+      while ((buff = get_next_line(fd)) != NULL)
+	{
+	  printf(">> %s\n", buff);
+	}
+    }
+}
 
 void	init(int ac, char **argv)
 {
@@ -14,7 +35,7 @@ void	init(int ac, char **argv)
 
   i = 1;
   while (i < ac)
-    printf("Initialisation de %s\n", argv[i++]);
+    tread_file(argv[i]);
 }
 
 int	main(int ac, char *argv[])
