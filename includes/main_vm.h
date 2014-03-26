4241f8aaa7e0d15633b31dff827097122e69e59d
merran_g <merran_g@epitech.eu>
2014-03-26 10:40:07 +0100

    des trucs

---
+++ includes/main_vm.h
@@ -0,0 +1,17 @@
+/*
+** main_vm.h for main_vm.h in /home/merran_g/work/c_elem/corewar/vm
+** 
+** Made by Geoffrey Merran
+** Login   <merran_g@epitech.net>
+** 
+** Started on  Tue Mar 25 17:19:00 2014 Geoffrey Merran
+** Last update Tue Mar 25 17:25:41 2014 Geoffrey Merran
+*/
+
+#ifndef MAIN_VM_
+# define MAIN_VM_
+# define VM_USAGE "Usage : ./corewar [-dump nbr_cycle] [[-n prog_number] [-a load_address ] prog_name]\n"
+# include "my.h"
+# include "my_printf.h"
+
+#endif /* MAIN_VM_ */
