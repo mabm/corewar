70e8aa672776e6a4ef390bad659c030499f98cbc
Nicolas ades <nicolas.ades@epitech.eu>
2014-03-27 11:46:18 +0100

    nico

--- vm/main.c
+++ vm/main.c
@@ -5,7 +5,7 @@
 ** Login   <merran_g@epitech.net>
 ** 
 ** Started on  Tue Mar 25 16:39:29 2014 Geoffrey Merran
-** Last update Wed Mar 26 11:40:43 2014 Nicolas Ades
+** Last update Thu Mar 27 11:13:27 2014 Nicolas Ades
 */
 
 #include "main_vm.h"
@@ -13,7 +13,7 @@
 int	main(int ac, char **av)
 {
   if (ac > 1)
-    vm_parse(av);
+    vm_parse(ac, av);
   else
     my_printf(VM_USAGE);
   return (0);
