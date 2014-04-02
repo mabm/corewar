454aedda6654b104377ed3916ad0e8416e22c316
Nicolas ades <nicolas.ades@epitech.eu>
2014-04-02 14:30:52 +0200

    paye ton conflit

---
+++ includes/vm_arena.h
@@ -0,0 +1,27 @@
+/*
+** vm_arena.h for  in /home/nicolas/Workspace/Corewar/includes
+** 
+** Made by Nicolas Ades
+** Login   <nicolas@epitech.net>
+** 
+** Started on  Tue Apr  1 11:50:43 2014 Nicolas Ades
+** Last update Tue Apr  1 11:58:38 2014 Nicolas Ades
+*/
+
+#ifndef VM_ARENA_H_
+# define VM_ARENA_H_
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "my.h"
+# include "my_printf.h"
+# include "op.h"
+
+void	init_battle(unsigned char *arena);
+
+#endif /* VM_ARENA_H_ */
--- vm/vm_arene.c
+++ vm/vm_arene.c
@@ -5,6 +5,18 @@
 ** Login   <nicolas@epitech.net>
 ** 
 ** Started on  Thu Mar 27 14:14:19 2014 Nicolas Ades
-** Last update Thu Mar 27 14:14:25 2014 Nicolas Ades
+** Last update Wed Apr  2 11:14:01 2014 Nicolas Ades
 */
 
+#include "vm_arena.h"
+
+void		init_battle(unsigned char *arena)
+{
+  int		i;
+
+  i = 0;
+  while (i++)
+    {
+      
+    }
+}
