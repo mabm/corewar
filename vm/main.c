8398bfd0307aa709f5e2497bba72ebcf96710a06
merran_g <merran_g@epitech.eu>
2014-04-03 13:20:32 +0200

    unsiegned

--- vm/main.c
+++ vm/main.c
@@ -5,7 +5,7 @@
 ** Login   <merran_g@epitech.net>
 ** 
 ** Started on  Tue Mar 25 16:39:29 2014 Geoffrey Merran
-** Last update Wed Apr  2 17:24:43 2014 Geoffrey Merran
+** Last update Thu Apr  3 12:47:03 2014 Geoffrey Merran
 */
 
 #include "main_vm.h"
@@ -56,7 +56,7 @@ void		init_all(int ac, char **av)
   init_id(&champs);
   init_addr(&champs);
   load_champs(&champs, arena);
-  /* aff_champ(champs); */
+  aff_champ(champs);
   aff_arena(arena);
   free(arena);
 }
