b447bc215bd7e346decf4dfa9b738cfdf13a9bca
merran_g <merran_g@epitech.eu>
2014-04-13 18:55:51 +0200

    commit

--- corewar/vm_arena3.c
+++ corewar/vm_arena3.c
@@ -5,15 +5,17 @@
 ** Login   <nicolas@epitech.net>
 ** 
 ** Started on  Sun Apr 13 07:24:08 2014 Nicolas Ades
-** Last update Sun Apr 13 18:12:32 2014 Geoffrey Merran
+** Last update Sun Apr 13 18:51:47 2014 Geoffrey Merran
 */
 
+#include "vm_arena.h"
 
 void		dump_arena(unsigned char *arena, int cycle_max)
 {
   int		i;
 
   i = 0;
+  my_printf("\n== DUMPING MEMORY ==\n");
   while (i <= MEM_SIZE)
     {
       if (arena[i] < 16)
@@ -23,5 +25,5 @@ void		dump_arena(unsigned char *arena, int cycle_max)
 	my_putchar('\n');
       i++;
     }
-  my_printf("Memory was dumped at %d cycle\n", cycle_max);
+  my_printf("\nMemory was dumped at %d cycle\n", cycle_max);
 }
