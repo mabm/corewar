aec859f6025b874133be913fb113030c16958253
Nicolas ades <nicolas.ades@epitech.eu>
2014-04-13 18:55:43 +0200

    nico

--- libmy/my_puterr.c
+++ libmy/my_puterr.c
@@ -5,13 +5,15 @@
 ** Login   <merran_g@epitech.net>
 ** 
 ** Started on  Tue Feb 18 12:16:38 2014 Geoffrey Merran
-** Last update Tue Feb 18 12:17:03 2014 Geoffrey Merran
+** Last update Sun Apr 13 18:51:33 2014 Nicolas Ades
 */
 
+#include <stdlib.h>
 #include <unistd.h>
 #include "my.h"
 
 void	my_puterr(char *err)
 {
-  write(2, err, my_strlen(err));
+  if (write(2, err, my_strlen(err)) == -1)
+    exit(-1);
 }
